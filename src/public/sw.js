503ff2e94426845bd28fd80f1dafe1f797f3150e
lucasearce-collab <lucas.e.arce@gmail.com>
2026-04-24 19:38:53 -0500

    Create sw.js

---
+++ src/public/sw.js
@@ -0,0 +1,22 @@
+{
+  "name": "Pendientes",
+  "short_name": "Pendientes",
+  "description": "Tu gestor de tareas y metas",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#F7F5F2",
+  "theme_color": "#F7F5F2",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
